eb9ee0e5e6f3c20106bf06987e19eebcd9c6d846
Kullanıcı Adınız <email@adresiniz.com>
2023-08-05 02:30:21 +0300

    sidebar css düznelendi iconlar eklendi

--- sidebar.js
+++ sidebar.js
@@ -1,28 +1,48 @@
 import HomeIcon from "@mui/icons-material/Home";
-import TimelineIcon from "@mui/icons-material/Timeline";
+import BoltIcon from "@mui/icons-material/Bolt";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import SlideshowIcon from "@mui/icons-material/Slideshow";
+import HistoryIcon from "@mui/icons-material/History";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LocalFireDepartmentIcon from "@mui/icons-material/LocalFireDepartment";
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import SensorsIcon from "@mui/icons-material/Sensors";
+import VideogameAssetIcon from "@mui/icons-material/VideogameAsset";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import SettingsIcon from "@mui/icons-material/Settings";
+import FlagIcon from "@mui/icons-material/Flag";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
+import SmsFailedIcon from "@mui/icons-material/SmsFailed";
 export const Menu = [
-  { name: "ana_sayfa", icon: <HomeIcon /> },
-  { name: "shorts", icon: <TimelineIcon /> },
-  { name: "abonelikler", icon: <HomeIcon /> },
-  { name: "kitaplik", icon: <HomeIcon /> },
-  { name: "gecmis", icon: <HomeIcon /> },
-  { name: "videolar_begen", icon: <HomeIcon /> },
-  { name: "yorum_yap", icon: <HomeIcon /> },
-  { name: "abone_ol", icon: <HomeIcon /> },
-  { name: "oturum_ac", icon: <HomeIcon /> },
-  { name: "kesfet", icon: <HomeIcon /> },
-  { name: "trendler", icon: <HomeIcon /> },
-  { name: "muzik", icon: <HomeIcon /> },
-  { name: "canli", icon: <HomeIcon /> },
-  { name: "oyun", icon: <HomeIcon /> },
-  { name: "spor", icon: <HomeIcon /> },
-  { name: "kanallar", icon: <HomeIcon /> },
-  { name: "daha_fazla_icerik", icon: <HomeIcon /> },
-  { name: "premium", icon: <HomeIcon /> },
-  { name: "muzik", icon: <HomeIcon /> },
-  { name: "cocuklar", icon: <HomeIcon /> },
-  { name: "ayarlar", icon: <HomeIcon /> },
-  { name: "icerik_bildirme_gecmisi", icon: <HomeIcon /> },
-  { name: "yardim", icon: <HomeIcon /> },
-  { name: "geri_bildirim", icon: <HomeIcon /> },
+  { name: "Ana Sayfa", icon: <HomeIcon /> },
+  { name: "Shorts", icon: <BoltIcon /> },
+  { name: "Abonelikler", icon: <SubscriptionsIcon /> },
+  { name: "Kitaplik", icon: <SlideshowIcon /> },
+  { name: "Gecmis", icon: <HistoryIcon /> },
+  {
+    name: "Videoları beğenmek, yorum yapmak ve abone olmak için oturum açın.",
+    icon: null,
+  },
+  { name: "Kesfet", icon: null },
+  { name: "Trendler", icon: <LocalFireDepartmentIcon /> },
+  { name: "Muzik", icon: <MusicNoteIcon /> },
+  { name: "Canli", icon: <SensorsIcon /> },
+  { name: "Oyun", icon: <VideogameAssetIcon /> },
+  { name: "Spor", icon: <EmojiEventsIcon /> },
+  { name: "Kanallara göz at", icon: <AddCircleOutlineIcon /> },
+  { name: "YouTube'dan daha fazla icerik", icon: null },
+  { name: "YouTube Premium", icon: <YouTubeIcon /> },
+  { name: "YouTube Muzik", icon: <PlayCircleIcon /> },
+  { name: "YouTube Çocuklar", icon: <YouTubeIcon /> },
+  { name: "Ayarlar", icon: <SettingsIcon /> },
+  { name: "İcerik bildirme gecmisi", icon: <FlagIcon /> },
+  { name: "Yardim", icon: <HelpOutlineIcon /> },
+  { name: "Geri bildirim gönder", icon: <SmsFailedIcon /> },
+  {
+    name: "HakkındaBasınTelif hakkıBize ulaşınİçerik ÜreticilerReklam vermeGeliştiriciler ŞartlarGizlilikPolitika ve GüvenlikYouTube Nasıl Çalışıyor?Yeni özellikleri deneyin © 2023 Google LLC",
+    icon: null,
+  },
 ];
